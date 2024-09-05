b82cd3bb7dc282875167fc4940f61bf11679c8d8
nicola <148383343+wx3r@users.noreply.github.com>
2024-09-05 14:12:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿void Main(string[] args)
+{
+    void WhatItem()
+    {
+        Console.WriteLine("What's the title of the item you want to find?");
+        string userQuery = Console.ReadLine();
+    }
+}
