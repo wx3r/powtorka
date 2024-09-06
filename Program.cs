c35ec78476198a18a48b9d5f85c4f1c09e9abb06
nicola <148383343+wx3r@users.noreply.github.com>
2024-09-06 08:34:30 +0200

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿void Main(string[] args)
-{
-    void WhatItem()
-    {
-        Console.WriteLine("What's the title of the item you want to find?");
-        string userQuery = Console.ReadLine();
-    }
-}
